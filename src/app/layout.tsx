d37d8a8fd82eab8c31230ec7be8ff94492fbbfe7
yym-yumeng123 <18026493873@163.com>
2024-01-22 15:30:44 +0800

    install nextui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs"
 import "./globals.css"
+import { Providers } from "./providers"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,8 +19,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <header>Layout Header</header>
-        <ClerkProvider>{children}</ClerkProvider>
+        <ClerkProvider>
+          <Providers>{children}</Providers>
+        </ClerkProvider>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image"
-import { UserButton } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs"
+import { Button } from "@nextui-org/react"
 
 export default function Home() {
   return (
     <div>
       <h1>Hello</h1>
-      <UserButton afterSignOutUrl="/" />
+      <UserButton afterSignOutUrl='/' />
+      <Button color='primary'>Click me</Button>
     </div>
   )
 }
