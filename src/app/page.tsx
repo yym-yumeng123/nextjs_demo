0dea33d8ad3239bfc548178a6d59068a40c040ef
yym-yumeng123 <18026493873@163.com>
2024-01-22 15:22:10 +0800

    next file routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <div>
+      <h1>Hello</h1>
       <UserButton afterSignOutUrl="/" />
-    </main>
+    </div>
   )
 }
